24a50a3f3ce36944c367aa8963fae9bbf9916c65
rfreno <wintersbek@gmail.com>
2022-09-21 14:46:52 -0600

    added css

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+app.get('/css', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.css'))
+})
+
 
 app.listen(port, () => {
     console.log('Listening at port ' + port)
