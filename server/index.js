647105f3b5c315f47d3d9d30583b5cc22fd09f90
rfreno <wintersbek@gmail.com>
2022-09-21 14:26:57 -0600

    updated app.get

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 
-
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 
 app.listen(port, () => {
