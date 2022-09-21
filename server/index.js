3d997aa2554e67e6eb6636bbef78fecd6574afb8
rfreno <wintersbek@gmail.com>
2022-09-21 14:54:22 -0600

    updated css link

--- server/index.js
+++ server/index.js
@@ -9,17 +9,17 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+// app.use('/', express.static(path.join(__dirname, '../client/index.html')))
 
-app.use(express.static(path.join(__dirname, '../client')))
+// app.use(express.static(path.join(__dirname, '../client')))
 
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../client/index.html'))
-// })
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
-// app.get('/css', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../client/index.css'))
-// })
+app.get('/css', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.css'))
+})
 
 app.listen(port, () => {
     console.log('Listening at port ' + port)
