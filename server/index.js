71124b1f3b4ab397fa791d54e2a9b12ee31761fd
rfreno <wintersbek@gmail.com>
2022-09-22 14:20:39 -0600

    added rollbar

--- server/index.js
+++ server/index.js
@@ -9,13 +9,21 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 
-// app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '996ba0068cd94fdaa63b51274a1f2c17',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
-// app.use(express.static(path.join(__dirname, '../client')))
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+
 
 app.get('/css', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/index.css'))
