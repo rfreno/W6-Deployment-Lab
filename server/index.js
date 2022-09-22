ba020ab7820d1cab3e421cf63a993da559b67a86
rfreno <wintersbek@gmail.com>
2022-09-22 14:42:22 -0600

    added step 2

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 
 const path = require('path')
 
@@ -8,6 +9,8 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(cors())
+
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require("rollbar");
