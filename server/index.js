a6545f3c38c6ca53bc3e58835a904c503ef2bbe2
rfreno <wintersbek@gmail.com>
2022-09-22 14:32:31 -0600

    added step 2

--- server/index.js
+++ server/index.js
@@ -41,6 +41,51 @@ app.get('/fake', (req, res) => {
     }
 })
 
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           rollbar.log('student was added successfully')
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.error('submission was empty')
+           res.status(400).send('You must enter a name.')
+       } else {
+            rollbar.error('student name already exists')
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    rollbar.info('Student was deleted')
+    res.status(200).send(students)
+})
+
+
+
+
 app.use('/js', express.static(path.join(__dirname, './index.js')))
 
 app.listen(port, () => {
